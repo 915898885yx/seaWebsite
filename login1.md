ccc7a9bb9dd48b1b65ee71f42dec95490cd69b18
wangyaxin2840 <yaxin.wang@eeoa.com>
2024-08-06 20:21:39 +0800

    暂存

---
+++ login1.md
@@ -0,0 +1,110 @@
+{
+    "authCallerContext": "",
+    "email": "wvaiyr47381@chacuo.net",
+    "phoneNumberLastDigits": null,
+    "authFactors": [
+        {
+            "name": "email"
+        },
+        {
+            "name": "password"
+        },
+        {
+            "name": "reset_password"
+        },
+        {
+            "name": "magic_link"
+        }
+    ],
+    "availableAuthFactors": [],
+    "enrolledAuthFactors": [
+        {
+            "name": "email"
+        },
+        {
+            "name": "password"
+        },
+        {
+            "name": "reset_password"
+        },
+        {
+            "name": "magic_link"
+        }
+    ],
+    "enrollableAuthFactors": [],
+    "consent": {
+        "hasAcceptedTerms": true,
+        "hasOptedInToMarketing": false
+    },
+    "webAuthnStatus": false,
+    "step": "password",
+    "canSignInAsAdmin": false,
+    "loginURL": "\/v\/account\/authentication\/login",
+    "transactionId": "I+F9OmayD6MVr8AEng+fAg==",
+    "applicationConstants": {
+        "TRANSACTION_ID": "I+F9OmayD6MVr8AEng+fAg==",
+        "IS_DEV": false,
+        "IMAGE_KEY": "silt-saLrTfxX*WAiLMp",
+        "CDN_URL": "https:\/\/assets.wfcdn.com\/",
+        "STORE_URL": "wayfair.com",
+        "IS_CA_FRENCH": false,
+        "WAYFAIR_ID": 49,
+        "WAYFAIR_UK_ID": 321,
+        "WAYFAIR_DE_ID": 368,
+        "WAYFAIR_CA_ID": 446,
+        "ALL_MODERN_ID": 81,
+        "BIRCH_LANE_ID": 422,
+        "JOSSANDMAIN_ID": 450,
+        "PERIGOLD_ID": 457,
+        "STORE_ID": 49,
+        "LOCALE": "en",
+        "CSRF_TOKEN": "0BDFB954DDD8AD40DEA9CAF71B5EB045"
+    },
+    "createURL": "\/v\/account\/authentication\/create",
+    "showMagicLink": true,
+    "destinationURL": "https:\/\/www.wayfair.com\/session\/secure\/account\/account_welcome.php",
+    "redirect": false,
+    "showRecaptcha": false,
+    "isFromCheckout": false,
+    "isFromRegistry": false,
+    "isAccountLocked": false,
+    "accountLockType": "",
+    "tempLockReason": "",
+    "customerSupportNumber": "8662638325",
+    "customerSupportNumberDisplay": "866-263-8325",
+    "minPasswordLength": 6,
+    "isGDPRStorefrontEnabled": false,
+    "isGDPRAcquisitionEnabled": false,
+    "isClientPhone": false,
+    "customerIsOptedIn": false,
+    "isActivatedOrAcquired": false,
+    "softOptIn": false,
+    "emailErrorType": "",
+    "passwordErrorType": "",
+    "hasOtherError": false,
+    "recaptchaSiteKey": "6Le6sQkUAAAAABJ5RHfFVnVNUGl2NeLpz_9MPJSO",
+    "showSmsAuth": true,
+    "enableSmsV2Api": true,
+    "inPostAuthSmsEnrollmentTest": false,
+    "action": "",
+    "mfaCode": "",
+    "mfaAllowedAlternateAuthFactors": [],
+    "applicationConfiguration": {
+        "enableSmsLogin": false,
+        "enableAuthSmsLoginAsDefaultOption": true,
+        "showLoadingStateOnSuccess": true
+    },
+    "showEmailOtp": true,
+    "preferredAuthFactor": "email",
+    "accountLockStatus": {
+        "type": null,
+        "reason": null
+    },
+    "isCcpaConsentManagementEnabled": true,
+    "isSecureMyAccountPasswordlessEnabled": true,
+    "resetPasswordViaEmailOtp": true,
+    "isRecoveryPickerRedesignEnabled": true,
+    "isLoginViewRedesignEnabled": false,
+    "errors": [],
+    "template_name": null
+}
--- src/index1.js
+++ src/index1.js
@@ -21,12 +21,13 @@ function getCookie () {
     }
     return result.join(';')
 }
-async function getXparentTxid() {
+async function getXparentTxid(options) {
+  const { email } = options
   const url = "https://www.wayfair.com/v/account/authentication/login";
-  const queryUrl = `https://www.wayfair.com/v/account/welcome/show?logout=${
-    Math.ceil(Date.now() / 1000) - 70
-  }`;
-  const target = `${url}?url=${encodeURIComponent(queryUrl)}`;
+//   const queryUrl = `https://www.wayfair.com/v/account/welcome/show?logout=${
+//     Math.ceil(Date.now() / 1000) - 70
+//   }`;
+//   const target = `${url}?url=${encodeURIComponent(queryUrl)}`;
   const myHeaders = new Headers();
   myHeaders.append(
     "accept",
@@ -36,7 +37,7 @@ async function getXparentTxid() {
   myHeaders.append("cache-control", "no-cache");
   myHeaders.append("pragma", "no-cache");
   myHeaders.append("priority", "u=0, i");
-  myHeaders.append("referer", queryUrl);
+  myHeaders.append("referer", url);
   myHeaders.append(
     "sec-ch-ua",
     '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"'
@@ -52,26 +53,30 @@ async function getXparentTxid() {
     "user-agent",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
   );
-  //myHeaders.append("Cookie", "CSNPersist=page_of_visit%3D2; CSNUtId=23e17d3a-66b1-bb13-aec7-adb7dd03ef02; CSN_CSRF=058439ec8dd28ce54dd4ee23fb16a577562733a875075d148a340fcd4a97a544; ExCSNUtId=23e17d3a-66b1-bb13-aec7-adb7dd03ef02; SFSID=ea110755f42a1403c356bb510aec7709; WFDC=DSM; __cf_bm=NO65EP3sY7H5MpI1quOB8Tug90PQe7G6.kv5b2YRjzA-1722923795-1.0.1.1-PojWpEFbPCgEslEg9o7kcd3mk_2jLwQywoR4z7RD88sOj4S9EY.p.cDbyLJnmoV00p6od3R1FTYSH8NYgliKfA; canary=0; serverUAInfo=%7B%22browser%22%3A%22Google%20Chrome%22%2C%22browserVersion%22%3A125%2C%22OS%22%3A%22Mac%20OS%20X%22%2C%22OSVersion%22%3A%22%22%2C%22isMobile%22%3Afalse%2C%22isTablet%22%3Afalse%2C%22isTouch%22%3Afalse%7D; vid=23e17d3a-66b1-bb13-aec7-adb7dd03ef02");
-
+  const raw = JSON.stringify({
+    email
+  })
   const requestOptions = {
     method: "GET",
     headers: myHeaders,
     redirect: "follow",
+    // body: raw
   };
-
-  fetch(target, requestOptions)
+  // transactionId
+  fetch(url, requestOptions)
     .then((response) => {
       if (response.status === 200) {
         const cookies = response.headers.get("Set-Cookie");
+        dealCookie(cookies);
         console.log(cookies, "cookies", response.status);
+        console.log(response.json(), 'json')
         return response.text();
       }
       console.log(response.status);
     })
     .catch((error) => console.error(error));
 }
-// getXparentTxid()
+getXparentTxid({ email: 'wvaiyr47381@chacuo.net' })
 
 async function login(options) {
   const { email, password, cookie } = options;
@@ -85,7 +90,7 @@ async function login(options) {
   myHeaders.append("priority", "u=1, i");
   myHeaders.append(
     "referer",
-    "https://www.wayfair.com/v/account/authentication/login?url=https%3A%2F%2Fwww.wayfair.com%2Fv%2Faccount%2Fwelcome%2Fshow%3Flogout%3D1722912391"
+    "https://www.wayfair.com/v/account/authentication/login"
   );
   myHeaders.append(
     "sec-ch-ua",
@@ -103,10 +108,11 @@ async function login(options) {
   myHeaders.append("x-auth-caller-context", "auth_main_page_context");
   myHeaders.append("x-parent-txid", "I/cmXGaxkB6akobBl9s/Ag==");
   myHeaders.append("x-requested-with", "XMLHttpRequest");
-  myHeaders.append(
-    "Cookie",
-    "CSNID=593B940E-B247-4541-B195-ADB34021F5F8; CSNUtId=23e17d3a-66b1-9e44-67f7-ca7c62234702; CSN_CSRF=9d15c4bf0a86e376969340fadd626dd3f08d712468e94929b482d10aac236f05; CSN_CT=eyJhbGciOiJSUzI1NiIsImtpZCI6InBlRG5BMWVVVGRVQU00YVdjU3FnZm40ZEJhbFZCYnJ4R2ZEU0ZQYXVQbG8iLCJ0eXAiOiJKV1QiLCJ4NXQiOiJtM2Y3ZHY4OFctQXA5dE5UZFVZYmxEbXc5TUUiLCJqa3UiOiJodHRwczovL3Nzby5hdXRoLndheWZhaXIuY29tLy53ZWxsLWtub3duL29wZW5pZC1jb25maWd1cmF0aW9uL2p3a3MifQ.eyJuYmYiOjE3MjI5MTY0MjAsImV4cCI6MTcyMjkzMDgyMSwiaWF0IjoxNzIyOTE2NDIxLCJpc3MiOiJodHRwczovL3Nzby5hdXRoLndheWZhaXIuY29tLyIsImF1ZCI6ImF1dGhfaWRlbnRpdHkiLCJjbGllbnRfaWQiOiJ5NnZVV0dUd2dFVlA0VEFZbXZuTGxBIiwiY3VpZCI6IjYxMDg5MTQ3NzYiLCJzdWIiOiIyOTQ4MDY2MjQiLCJ2ZXJzaW9uIjoiMi4wIiwicGx0Ijoid2ViIiwicm9sZSI6InNmOmN1c3RvbWVyIiwianRpIjoiN0QwRjA2NjA1NzY2NzA2NTVEMzgyMzRBNUExN0Y5RDYiLCJzY29wZSI6Im9wZW5pZCBzZjpyZWNvZ25pemVkIHNmOnRydXN0ZWQgc2Y6dmVyaWZpZWQgb2ZmbGluZV9hY2Nlc3MiLCJhbXIiOlsicGFzc3dvcmQiXX0.e_yCcupUFrmYi0CnIFVrPWwWGe-hPwLW4qx368_OHxTwmD38kcwlH4amWUAimCi2Utyx2iVl6HEQVirdXEvNwnOJzMiHaApAqcQtXwEfFHzsvz4H5NDgVI2R86-aoJ44aRY4YGjjrLfTZv6hoFGx-91BR3Kcfga6TiAJzkRA5-l1fhmDkB3hsf0lYiPqI_9rj-b4PlY0ivRtwQC9ezWliGHzcE1ol574A7YyYap06LsRvgiF2blJut8Rwl8t2U7jNNU1bvgmRbr0UyFRABASnAlhtMr2DuESlgnUSiDqqCeq1_ppySPa-mt7ZGOVnZqEjvtmwYoeQFtNMukm_bKS_1UiqywtCulef6jfLOv3-k21d8FIzCGonaGl2N_ngghILtijgtqLKT2rT50XYUdS1kfwVkOv3nKWfAq8IZAXebW3FCMI47r9pekKy9ENF4WWpq0dnha_AqzWDrNhrw7o9yTSFo02HwNTGaDvy9tHdGgywh503FXrV4vOkzraJGiyxYoTaoYqvLqG92c-wTVBjk-FbmacbPftpeFvmjuHpnH6MW_jAWhklNapwGxszWHBJpYXqmlCVO1T3Q58E80SlWA5eW2Wrr3e6uaK-BWT9jmPyOJOZKiVdQqnMvmtAzs3TIyZsRxwKO94iRS9WKaKnyAg8EMUYWlEIL-dFroTK9c; CSN_RT=9EAAB196F4AF1FBD5BC21189E8ACA32E6A59CA15E1034C826B4CB5817F15CE86; ExCSNUtId=23e17d3a-66b1-9e44-67f7-ca7c62234702; SFSID=ff3f837e1efe81aaf584206788ee89ca; WFDC=DSM; __cf_bm=3ctEJXCpZmGYu2b1WaCOaKFBM3qA3eUzEcN6i.sL5LU-1722916422-1.0.1.1-Wt1R61VbutwehxDC_rC09YKG5iguMm9JJNGrEnCIi3zglsTcVDJT7bUqd0LgFpjAC8OtgszzsPmflJzFi_2aXg; canary=0; postalCode=67346; serverUAInfo=%7B%22browser%22%3A%22Google%20Chrome%22%2C%22browserVersion%22%3A125%2C%22OS%22%3A%22Mac%20OS%20X%22%2C%22OSVersion%22%3A%22%22%2C%22isMobile%22%3Afalse%2C%22isTablet%22%3Afalse%2C%22isTouch%22%3Afalse%7D; vid=23e17d3a-66b1-9e44-67f7-ca7c62234702"
-  );
+//   myHeaders.append(
+//     "Cookie",
+//     "CSNID=593B940E-B247-4541-B195-ADB34021F5F8; CSNUtId=23e17d3a-66b1-9e44-67f7-ca7c62234702; CSN_CSRF=9d15c4bf0a86e376969340fadd626dd3f08d712468e94929b482d10aac236f05; CSN_CT=eyJhbGciOiJSUzI1NiIsImtpZCI6InBlRG5BMWVVVGRVQU00YVdjU3FnZm40ZEJhbFZCYnJ4R2ZEU0ZQYXVQbG8iLCJ0eXAiOiJKV1QiLCJ4NXQiOiJtM2Y3ZHY4OFctQXA5dE5UZFVZYmxEbXc5TUUiLCJqa3UiOiJodHRwczovL3Nzby5hdXRoLndheWZhaXIuY29tLy53ZWxsLWtub3duL29wZW5pZC1jb25maWd1cmF0aW9uL2p3a3MifQ.eyJuYmYiOjE3MjI5MTY0MjAsImV4cCI6MTcyMjkzMDgyMSwiaWF0IjoxNzIyOTE2NDIxLCJpc3MiOiJodHRwczovL3Nzby5hdXRoLndheWZhaXIuY29tLyIsImF1ZCI6ImF1dGhfaWRlbnRpdHkiLCJjbGllbnRfaWQiOiJ5NnZVV0dUd2dFVlA0VEFZbXZuTGxBIiwiY3VpZCI6IjYxMDg5MTQ3NzYiLCJzdWIiOiIyOTQ4MDY2MjQiLCJ2ZXJzaW9uIjoiMi4wIiwicGx0Ijoid2ViIiwicm9sZSI6InNmOmN1c3RvbWVyIiwianRpIjoiN0QwRjA2NjA1NzY2NzA2NTVEMzgyMzRBNUExN0Y5RDYiLCJzY29wZSI6Im9wZW5pZCBzZjpyZWNvZ25pemVkIHNmOnRydXN0ZWQgc2Y6dmVyaWZpZWQgb2ZmbGluZV9hY2Nlc3MiLCJhbXIiOlsicGFzc3dvcmQiXX0.e_yCcupUFrmYi0CnIFVrPWwWGe-hPwLW4qx368_OHxTwmD38kcwlH4amWUAimCi2Utyx2iVl6HEQVirdXEvNwnOJzMiHaApAqcQtXwEfFHzsvz4H5NDgVI2R86-aoJ44aRY4YGjjrLfTZv6hoFGx-91BR3Kcfga6TiAJzkRA5-l1fhmDkB3hsf0lYiPqI_9rj-b4PlY0ivRtwQC9ezWliGHzcE1ol574A7YyYap06LsRvgiF2blJut8Rwl8t2U7jNNU1bvgmRbr0UyFRABASnAlhtMr2DuESlgnUSiDqqCeq1_ppySPa-mt7ZGOVnZqEjvtmwYoeQFtNMukm_bKS_1UiqywtCulef6jfLOv3-k21d8FIzCGonaGl2N_ngghILtijgtqLKT2rT50XYUdS1kfwVkOv3nKWfAq8IZAXebW3FCMI47r9pekKy9ENF4WWpq0dnha_AqzWDrNhrw7o9yTSFo02HwNTGaDvy9tHdGgywh503FXrV4vOkzraJGiyxYoTaoYqvLqG92c-wTVBjk-FbmacbPftpeFvmjuHpnH6MW_jAWhklNapwGxszWHBJpYXqmlCVO1T3Q58E80SlWA5eW2Wrr3e6uaK-BWT9jmPyOJOZKiVdQqnMvmtAzs3TIyZsRxwKO94iRS9WKaKnyAg8EMUYWlEIL-dFroTK9c; CSN_RT=9EAAB196F4AF1FBD5BC21189E8ACA32E6A59CA15E1034C826B4CB5817F15CE86; ExCSNUtId=23e17d3a-66b1-9e44-67f7-ca7c62234702; SFSID=ff3f837e1efe81aaf584206788ee89ca; WFDC=DSM; __cf_bm=3ctEJXCpZmGYu2b1WaCOaKFBM3qA3eUzEcN6i.sL5LU-1722916422-1.0.1.1-Wt1R61VbutwehxDC_rC09YKG5iguMm9JJNGrEnCIi3zglsTcVDJT7bUqd0LgFpjAC8OtgszzsPmflJzFi_2aXg; canary=0; postalCode=67346; serverUAInfo=%7B%22browser%22%3A%22Google%20Chrome%22%2C%22browserVersion%22%3A125%2C%22OS%22%3A%22Mac%20OS%20X%22%2C%22OSVersion%22%3A%22%22%2C%22isMobile%22%3Afalse%2C%22isTablet%22%3Afalse%2C%22isTouch%22%3Afalse%7D; vid=23e17d3a-66b1-9e44-67f7-ca7c62234702"
+//   );
+  myHeaders.append('Cookie', getCookie());
 
   const raw = JSON.stringify({
     email,
